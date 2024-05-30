7c23c1044f23042125161f9999ee5291582f92ee
unknown <paesguigo@gmail.com>
2024-05-30 19:46:11 -0300

    Treino com rotas

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
 const express = require("express");
+
+const app = express();
+
+app.use(express.json());
+
+const log = (req, res, next) => {
+  console.log(req.body);
+  console.log(`DATA: ${Date.now()}`);
+  next();
+};
+
+app.use(log);
+
+app.get("/", (req, res) => {
+  res.send("<h1> Minha lista de tarefas :)<h1>");
+});
+
+app.get("/json", (req, res) => {
+  res.json({ title: "Tarefa-X", done: true, date: "26/07/2007" });
+});
+
+app.listen(3000, () => {
+  console.log(`Servidor iniciado`);
+});
