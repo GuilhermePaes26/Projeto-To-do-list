856bf4680cb3d9511bae40389eb54d55821531cd
unknown <paesguigo@gmail.com>
2024-07-01 11:34:54 -0300

    Adição de novos arquivos

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const checklistRouter = require("./src/routes/checklist");
-
+const mongoose = require("./config/database");
 const app = express();
 app.use(express.json());
 
