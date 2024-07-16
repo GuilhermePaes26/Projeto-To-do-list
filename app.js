6a3e0b754c4245a1642d957fffb617acc55ea7d1
unknown <paesguigo@gmail.com>
2024-07-15 21:03:59 -0300

    Adição de novas funcionalidades com as routas e o ejs

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 const express = require("express");
 const checklistRouter = require("./src/routes/checklist");
+const rootRouter = require("./src/routes/index");
 const mongoose = require("./config/database");
+const path = require("path");
 const app = express();
 app.use(express.json());
 
+app.set("views", path.join(__dirname, "src/views"));
+app.set("view engine", "ejs");
+
 app.use("/checklists", checklistRouter);
+app.use("/", rootRouter);
+app.use(express.static(path.join(__dirname, "public")));
 app.listen(3000, () => {
   console.log(`Servidor iniciado`);
 });
--- src/routes/checklist.js
+++ src/routes/checklist.js
@@ -1,29 +1,56 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  console.log("Hello");
-  res.send();
+const Checklist = require("../models/checklist");
+const checklist = require("../models/checklist");
+// const checklist = require("../models/checklist");
+
+router.get("/", async (req, res) => {
+  try {
+    let checklists = await Checklist.find({});
+    res.status(200).render("checklists/index", { checklists: checklists });
+  } catch (error) {
+    res.status(500).render("pages/error", { error: "Erro ao exibir as listas" });
+  }
 });
 
-router.post("/", (req, res) => {
-  console.log(req.body);
-  res.status(200).send(req.body);
+router.post("/", async (req, res) => {
+  let { name } = req.body;
+  try {
+    let checklist = await Checklist.create({ name });
+    res.status(200).json(checklist);
+  } catch (error) {
+    res.status(422).json(error);
+  }
 });
 
-router.get("/:id", (req, res) => {
-  console.log(`ID:${req.params.id}`);
-  res.status(200).send(`ID:${req.params.id}`);
+router.get("/:id", async (req, res) => {
+  // 6687f3e37e7a4944a9904742
+  try {
+    let checklist = await Checklist.findById(req.params.id);
+    res.status(200).render("checklists/show", { checklist: checklist });
+  } catch (error) {
+    res.status(500).render("pages/error", { error: "Erro ao exibir as listas de tarefas" });
+  }
 });
 
-router.put("/:id", (req, res) => {
-  console.log(`PUT ID:${req.params.id}`);
-  res.status(200).send(req.params.id);
+router.put("/:id", async (req, res) => {
+  let { name } = req.body;
+  try {
+    let checklist = await Checklist.findByIdAndUpdate(req.params.id, { name }, { new: true });
+    res.status(200).json(checklist);
+  } catch (error) {
+    res.status(422).json(error);
+  }
 });
 
-router.delete("/:id", (req, res) => {
-  console.log(`DELETE ID:${req.params.id}`);
-  res.status(200).send(req.params.id);
+router.delete("/:id", async (req, res) => {
+  try {
+    let checklist = await Checklist.findByIdAndDelete(req.params.id);
+    res.status(200).json(checklist);
+  } catch (error) {
+    res.status(422).json(error);
+  }
 });
 
 module.exports = router;
