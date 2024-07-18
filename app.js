c3a433c995d1169001ac0f50ed72cd8a2c159495
unknown <paesguigo@gmail.com>
2024-07-18 11:24:23 -0300

    Adição de novas rotas, tarefas e metódos

--- app.js
+++ app.js
@@ -1,15 +1,26 @@
 const express = require("express");
+
 const checklistRouter = require("./src/routes/checklist");
+const taskRouter = require("./src/routes/task");
+
 const rootRouter = require("./src/routes/index");
+
+const methodOverride = require("method-override");
 const mongoose = require("./config/database");
 const path = require("path");
 const app = express();
+
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method", { methods: ["POST", "GET"] }));
 
 app.set("views", path.join(__dirname, "src/views"));
 app.set("view engine", "ejs");
 
 app.use("/checklists", checklistRouter);
+app.use("/checklists", taskRouter.checklistDependent);
+app.use("/tasks", taskRouter.simple);
+
 app.use("/", rootRouter);
 app.use(express.static(path.join(__dirname, "public")));
 app.listen(3000, () => {
